8dd806a58010f64ab47ddf4779de5e9bb7bf171b
iya5 <175221759+iya5@users.noreply.github.com>
2025-08-25 14:26:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_appdev
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_appdev/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_appdev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
